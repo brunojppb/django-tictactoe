6bf72a58a0f51f2786859e8ba987bc39c7a4b976
Bruno Paulino <bruno@woombikes.com>
2018-09-21 13:02:00 +0200

    add signup form

--- gameplay/urls.py
+++ gameplay/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
 
 from .views import game_detail, make_move, AllGamesList
 
--- player/views.py
+++ player/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
 from .forms import InvitationForm
 from gameplay.models import Game
@@ -51,3 +54,9 @@ def accept_invitation(request, id):
         return redirect('player_home')
     else:
         return render(request, "player/accept_invitation_form.html", {'invitation': invitation})
+
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = "player/signup_form.html"
+    success_url = reverse_lazy('player_home')
